7c206bb56bec685273ae1f832d541f35e2ae20ee
Franziska-F <f.fouchs@gmail.com>
2022-04-21 23:23:33 +0200

    Add styling, tried different functions

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const colors = ["green", "red", "rgba(133,122,200), #f15020"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15020"];
 
 const button = document.getElementById("btn");
 
-const showColor = document.querySelector("color");
+const showColor = document.querySelector(".color");
 
-button.addEventListener(click, function () {
-  //get random number between 0-3
+/*let getColor = () => {
   const randomNumber = Math.floor(Math.random() * 4);
+  console.log(randomNumber);
   document.body.style.backgroundColor = colors[randomNumber];
-  colors.textContent = colors[randomNumber];
-});
+  showColor.textContent = colors[randomNumber];
+};*/
+
+button.addEventListener("click", getColor);
+
+function getColor() {
+  const randomNumber = Math.floor(Math.random() * 4);
+  console.log(randomNumber);
+  document.body.style.backgroundColor = colors[randomNumber];
+  showColor.textContent = colors[randomNumber];
+}
