6fc4e140899a1740b107af62890a670ec0736c39
Franziska-F <f.fouchs@gmail.com>
2022-04-25 10:42:06 +0200

    remove console.logs and add comment

--- hex.js
+++ hex.js
@@ -1,5 +1,5 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-console.log(hex);
+
 const button = document.getElementById("btn");
 
 const showColor = document.querySelector(".color");
@@ -9,8 +9,8 @@ function getHex() {
   let hexColor = "#";
 
   for (let i = 0; i < 6; i++) {
+    // i = counter var
     hexColor += hex[getRandomNumber()]; //+= adds numbers eg #0, #0B,
-    console.log(hexColor);
   }
 
   showColor.textContent = hexColor;
